e0e830fbd6b113b0bee18975fcadb1795c62debb
alvarengacarlos <carlosho200@hotmail.com>
2025-01-24 14:37:18 -0400

    Creates the CICD Updates the readmes Creates a basic frontend Unifies .gitignore files Updates infra to support frontend and backend services. Also, moves database to Amazon RDS service Updates backend to support undertow embedded server. Also, removes unnecessary configuration from pom.xml

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,7 +2,7 @@ variable "environment" {
   description = "Set the environment"
   type        = string
   validation {
-    condition     = var.environment == "dev" || var.environment == "prod"
+    condition     = can(regex("^(dev|prod)$", var.environment))
     error_message = "The 'environment' variable must be 'dev' or 'prod'"
   }
 }
@@ -14,4 +14,29 @@ variable "my_public_ip" {
     condition     = can(regex("^\\d+\\.\\d+\\.\\d+\\.\\d+\\/\\d+$", var.my_public_ip))
     error_message = "The 'your_public_ip' variable must be a valid IPv4 address"
   }
+}
+
+variable "aws_region" {
+  description = "Set the aws region"
+  type        = string
+  validation {
+    condition     = can(regex("^(us-east-1|us-east-2)$", var.aws_region))
+    error_message = "The 'aws_region' variable must be a valid region like 'us-east-1'"
+  }
+}
+
+variable "database_root_user_password" {
+  description = "Set the root user database password"
+  type        = string
+  sensitive   = true
+  validation {
+    condition     = can(regex("^[^/'\"@]*$", var.database_root_user_password))
+    error_message = "The password cannot contain / ' \" or @ characters"
+  }
+}
+
+variable "github_personal_access_token" {
+  description = "Set the github token allowing aws access the repository"
+  type        = string
+  sensitive   = true
 }
